4267a2cac400f4923ffdb3a1ca1a4ccd41846742
aaljin-dev <aaljin.dev@gmail.com>
2026-06-06 09:05:48 +0530

    home secandhalf

--- src/component/share/UserModifier.jsx
+++ src/component/share/UserModifier.jsx
@@ -1,25 +1,49 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 
 const UserModifier = ({ order }) => {
-  console.log(order.modifier);
+  const [openId, setOpenId] = useState(null);
 
-  if (!order?.modifier) {
-    return <div>Loading...</div>;
-  }
+  if (!order?.modifier) return <div>Loading...</div>;
+
+  console.log(order.modifier);
+  console.log(openId);
 
   return (
-    <div>
-      {order.modifier.map((item) => {
-        return (
-          <select key={item.id} className="w-full">
-            <option value="">{item.modifierinfo.mname}</option>
-            {item.modifierinfo?.option?.map((itemOption) => (
-              <option key={itemOption.id}>{itemOption.optionname}</option>
-            ))}
-          </select>
-        );
-      })}
+    <div className="space-y-4">
+      {order.modifier.map((modifier) => (
+        <div key={modifier.id}>
+          <div
+            className="flex justify-between items-center p-4 cursor-pointer bg-gray-100"
+            onClick={() =>
+              setOpenId(openId === modifier.id ? null : modifier.id)
+            }
+          >
+            <h3>
+              {modifier.modifierinfo.mname}
+              {modifier.modifierinfo.isrequired === 1 && (
+                <span className="text-red-500 ml-1">(Required)</span>
+              )}
+            </h3>
+            <span>{openId === modifier.id ? "-" : "+"}</span>
+          </div>
+          {openId === modifier.id && (
+            <div className="p-4">
+              {modifier.modifierinfo.option?.map((option) => (
+                <div key={option.id} className="flex justify-between py-2">
+                  <div>
+                    <input type="radio" name={`one-${modifier.id}`} />
+                    <span className="ml-2">{option.optionname}</span>
+                    {option.qoh == 0 && (
+                      <p className="text-red-500 text-sm">out of stock</p>
+                    )}
+                  </div>
+                  <span>{option.price} QAR</span>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      ))}
     </div>
   );
 };
