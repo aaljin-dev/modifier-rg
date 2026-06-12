d65fadd725d953cea9c61b14835d1cfc2dc8b1bb
Aaljin <aaljin.dev@gmail.com>
2026-06-12 18:52:39 +0530

    two

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const ModifierPage = async () => {
+  // jjhuhguhybguyguyguyhbuyhbuhbuyb
   try {
     const product = await axios.get(
       "https://multibranch.raregroup.info/api/v2/getSingleProduct/11805",
